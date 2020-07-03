4ca133e5c1afed33351ddd6391b1218038a60143
nathan-stamand <nathansaint810@gmail.com>
2020-07-03 15:56:28 -0500

    created demo to test Tone with event listener, #buildTriad, #sanitizeId, and #play

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 document.querySelectorAll("button").forEach(button => {
-    button.addEventListener("mousedown", function(e) {
+    button.addEventListener("click", function(e) {
         Tone.start()
-        play(this.textContent)
+        buildTriad(this)
     })
 })
 
+function buildTriad(root) {
+  const rootId = parseInt(root.id)
+  const third = document.getElementById(`${sanitizeId(rootId + 2)}`)
+  const fifth = document.getElementById(`${sanitizeId(rootId + 4)}`)
+  playTriad(root, third, fifth)
+}
+
+function playTriad(root, third, fifth) {
+    play(`${root.textContent[0]}4`)
+  if (root.id > 5) {
+    play(`${third.textContent[0]}5`)
+    play(`${fifth.textContent[0]}5`)
+  }
+  else if (root.id > 3 ) {
+    play(`${third.textContent[0]}4`)
+    play(`${fifth.textContent[0]}5`)
+  }
+  else {
+    play(`${root.textContent[0]}4`)
+    play(`${third.textContent[0]}4`)
+    play(`${fifth.textContent[0]}4`)
+  }
+}
+
+function sanitizeId(id) {
+  if (id > 7) {
+    let newId = id - 7
+    return newId
+  }
+  return id
+}
+
 function play(note) {
-  const synth = new Tone.PolySynth(2).toDestination()
-  
-  synth.triggerAttackRelease(`${note}4`, "8n");
+  const synth = new Tone.Synth().toDestination()
+  console.log(note)
+  synth.triggerAttackRelease(`${note}`, "8n");
 }
