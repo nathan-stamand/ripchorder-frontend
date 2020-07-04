a2f23bb2b738abc32040c79a509ff2488f06bf22
nathan-stamand <nathansaint810@gmail.com>
2020-07-04 17:04:38 -0500

    commented out demo code and added const app declaration at top

--- src/index.js
+++ src/index.js
@@ -1,44 +1,46 @@
-document.querySelectorAll("button").forEach(button => {
-    button.addEventListener("click", function(e) {
-        Tone.start()
-        buildTriad(this)
-    })
-})
+const app = new App
 
-function buildTriad(root) {
-  const rootId = parseInt(root.id)
-  const third = document.getElementById(`${sanitizeId(rootId + 2)}`)
-  const fifth = document.getElementById(`${sanitizeId(rootId + 4)}`)
-  playTriad(root, third, fifth)
-}
+// document.querySelectorAll("button").forEach(button => {
+//     button.addEventListener("click", function(e) {
+//         Tone.start()
+//         buildTriad(this)
+//     })
+// })
 
-function playTriad(root, third, fifth) {
-    play(`${root.textContent[0]}4`)
-  if (root.id > 5) {
-    play(`${third.textContent[0]}5`)
-    play(`${fifth.textContent[0]}5`)
-  }
-  else if (root.id > 3 ) {
-    play(`${third.textContent[0]}4`)
-    play(`${fifth.textContent[0]}5`)
-  }
-  else {
-    play(`${root.textContent[0]}4`)
-    play(`${third.textContent[0]}4`)
-    play(`${fifth.textContent[0]}4`)
-  }
-}
+// function buildTriad(root) {
+//   const rootId = parseInt(root.id)
+//   const third = document.getElementById(`${sanitizeId(rootId + 2)}`)
+//   const fifth = document.getElementById(`${sanitizeId(rootId + 4)}`)
+//   playTriad(root, third, fifth)
+// }
 
-function sanitizeId(id) {
-  if (id > 7) {
-    let newId = id - 7
-    return newId
-  }
-  return id
-}
+// function playTriad(root, third, fifth) {
+//     play(`${root.textContent[0]}4`)
+//   if (root.id > 5) {
+//     play(`${third.textContent[0]}5`)
+//     play(`${fifth.textContent[0]}5`)
+//   }
+//   else if (root.id > 3 ) {
+//     play(`${third.textContent[0]}4`)
+//     play(`${fifth.textContent[0]}5`)
+//   }
+//   else {
+//     play(`${root.textContent[0]}4`)
+//     play(`${third.textContent[0]}4`)
+//     play(`${fifth.textContent[0]}4`)
+//   }
+// }
 
-function play(note) {
-  const synth = new Tone.Synth().toDestination()
-  console.log(note)
-  synth.triggerAttackRelease(`${note}`, "8n");
-}
+// function sanitizeId(id) {
+//   if (id > 7) {
+//     let newId = id - 7
+//     return newId
+//   }
+//   return id
+// }
+
+// function play(note) {
+//   const synth = new Tone.Synth().toDestination()
+//   console.log(note)
+//   synth.triggerAttackRelease(`${note}`, "8n");
+// }
