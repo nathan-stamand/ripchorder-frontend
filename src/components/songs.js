2d1325bc22031018af619d7772bb7a28a13d1e7b
nathan-stamand <nathansaint810@gmail.com>
2020-07-04 17:06:27 -0500

    created constructor for Songss which sets adapter/songs array, then calls #fetchAndLoadSongs which is defined below constructor.

--- src/components/songs.js
+++ src/components/songs.js
@@ -1,3 +1,18 @@
 class Songs {
-  
+  constructor() {
+    this.adapter = new SongsAdapter;
+    this.songs = [];
+    // this.bindEventListeners()
+    this.fetchAndLoadSongs()
+  }
+
+  fetchAndLoadSongs() {
+    this.adapter.getSongs().then(songs => {
+      songs.data.forEach(song => this.songs.push(song));
+    })
+    .then(() => {
+      this.render()
+    })
+  }
+
 }
