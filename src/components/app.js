49c7675dc691229de860737b47bab3c1f483438b
nathan-stamand <nathansaint810@gmail.com>
2020-07-04 17:11:10 -0500

    created constructor to set sthis.songs to new Songs and call #allSongsEventListener automatically, then defined #allSongsEventListener to attach a listener to div#all-songs

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,14 @@
 class App {
-  
+  constructor() {
+    this.songs = new Songs;
+    this.allSongsEventListener()
+  }
+
+  allSongsEventListener() {
+    const allSongs = document.getElementById("all-songs")
+    const application = this;
+    allSongs.addEventListener("click", () => {
+      application.hideOrUnhideSongs()
+    })
+  }
 }
