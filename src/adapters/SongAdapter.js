d3c323829f0bff65938087eeeb395d143b1dc5ec
nathan-stamand <nathansaint810@gmail.com>
2020-07-05 13:09:01 -0500

    added constructor with baseUrl, added #getSong

--- src/adapters/SongAdapter.js
+++ src/adapters/SongAdapter.js
@@ -1,3 +1,9 @@
 class SongAdapter {
-  
+  constructor() {
+    this.baseUrl = 'http://localhost:3000/songs/';
+  }
+
+  getSong(id) {
+    return fetch(this.baseUrl + id).then(res => res.json())
+  }
 }
