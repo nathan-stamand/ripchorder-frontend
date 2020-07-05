41bf6042f6e9698827e6389a16757cef3f9a3777
nathan-stamand <nathansaint810@gmail.com>
2020-07-05 13:41:36 -0500

    deleted #createShowDeleteButtons

--- src/components/songs.js
+++ src/components/songs.js
@@ -18,16 +18,5 @@ class Songs {
   render() {
     const songList = document.getElementById("song-list")
     songList.innerHTML = this.songs.map(song => song.renderLi()).join('')
-    this.createShowDeleteButtons()
-  }
-
-  createShowDeleteButtons() {
-    const songList = document.getElementsByClassName('song')
-    for (const song of songList) {
-      const title = song.innerHTML
-      const deleteBtn = `<button class='delete-button' data-song='${song.id}'>DELETE</button>`
-      const showBtn = `<button class='show-button' data-song='${song.id}'>SHOW</button>`
-      song.innerHTML = `${title}<div class='button-container' id='${song.id}'>${deleteBtn}${showBtn}</div>`
-    }
   }
 }
