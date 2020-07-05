30545bcd6c5861a1c28216f58197eea764a5ea81
nathan-stamand <nathansaint810@gmail.com>
2020-07-05 14:05:06 -0500

    took over functions from songs - #renderLi renders a li item from the song instance, #createShowDeleteButtons creates show/delete buttons for all song li's

--- src/components/song.js
+++ src/components/song.js
@@ -2,6 +2,21 @@ class Song {
   constructor(songJson) {
     this.id = songJson.id;
     this.title = songJson.attributes.title;
+    this.chord_feeds = new ChordFeeds(songJson.attributes.chord_feeds);
     this.adapter = new SongAdapter;
+  }
+
+  renderLi() {
+    return `<li class="song" id="${this.id}">${this.title}</li>`
+  }
+
+  static createShowDeleteButtons() {
+    const songList = document.getElementsByClassName('song')
+    for (const song of songList) {
+      const title = song.innerHTML
+      const deleteBtn = `<button class='delete-button' data-song='${song.id}'>DELETE</button>`
+      const showBtn = `<button class='show-button' data-song='${song.id}'>SHOW</button>`
+      song.innerHTML = `${title}<div class='button-container' id='${song.id}'>${deleteBtn}${showBtn}</div>`
+    }
   }
 }
