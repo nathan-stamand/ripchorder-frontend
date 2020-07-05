2e12346ee465c5e47e6d87078d5ad50530948ef0
nathan-stamand <nathansaint810@gmail.com>
2020-07-05 13:05:32 -0500

    added constructor for Song's id, title, and adapter; created #fetchSong method

--- src/components/song.js
+++ src/components/song.js
@@ -1,5 +1,11 @@
 class Song {
-  constructor() {
-    
+  constructor(songJson) {
+    this.id = songJson.id;
+    this.title = songJson.attributes.title;
+    this.adapter = new SongAdapter;
+  }
+
+  fetchSong() {
+    this.adapter.getSong(id).then(song => console.log(song.data))
   }
 }
