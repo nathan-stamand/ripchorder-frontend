6c573c3c1320381b01fe2705ba5534b6a6dcc80f
nathan-stamand <nathansaint810@gmail.com>
2020-07-06 16:11:52 -0500

    created objects blankChordFeed and newSongParams with default info for when this.song is set to a new Song (these will be default parameters) and called display on the app's song.

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,18 @@
 class App {
   constructor() {
     this.songs = new Songs;
+    const blankChordFeed = {id: null, position: 1, chord_array: ''}
+    const newSongParams = {
+      id: null, attributes: {
+        title: null, 
+        key: 'C', 
+        mode: 'major', 
+        tempo: 120, 
+        custom_chords: null, 
+        chord_feeds: [blankChordFeed]
+      }
+    }
+    this.song = new Song(newSongParams)
+    this.song.display()  
   }
 }
