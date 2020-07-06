d7fd25e9969f857b163e317f2ce0c7decc602667
nathan-stamand <nathansaint810@gmail.com>
2020-07-06 13:36:40 -0500

    modified constructor to fix formatting of some items/add key, tempo, and mode, created #display to call #renderKeyChords, #addCustomChords, and #addChordFeeds, defined #addCustomChords

--- src/components/song.js
+++ src/components/song.js
@@ -2,8 +2,11 @@ class Song {
   constructor(songJson) {
     this.id = songJson.id;
     this.title = songJson.attributes.title;
-    this.chord_feeds = new ChordFeeds(songJson.attributes.chord_feeds);
-    this.custom_chords = songJson.attributes.custom_chords;
+    this.key = songJson.attributes.key;
+    this.mode = songJson.attributes.mode;
+    this.tempo = songJson.attributes.tempo;
+    this.chordFeeds = new ChordFeeds(songJson.attributes.chord_feeds);
+    this.customChords = songJson.attributes.custom_chords;
     this.adapter = new SongAdapter;
   }
 
@@ -18,6 +21,29 @@ class Song {
       const deleteBtn = `<button class='delete-button' data-song='${song.id}'>DELETE</button>`
       const showBtn = `<button class='show-button' data-song='${song.id}'>SHOW</button>`
       song.innerHTML = `${title}<div class='button-container' id='${song.id}'>${deleteBtn}${showBtn}</div>`
+    }
+  }
+
+  display() {
+    if (this.customChords) {
+      this.renderKeyChords()
+      this.addCustomChords()
+    }
+    this.chordFeeds.addChordFeeds()
+  }
+
+  addCustomChords() {
+    const newChord = document.getElementById('new-chord')
+    const container = document.getElementById('chords-container')
+    const ccArray = this.customChords.split(', ')
+    for (const chord of ccArray) {
+      const chordBtn = document.createElement('button')
+      const idNum = document.getElementById('chords-container').childElementCount.length -1
+      chordBtn.className = 'chord'
+      chordBtn.id = idNum
+      chordBtn.textContent = chord
+      chordBtn.setAttribute('data-song', chord)
+      container.insertBefore(chordBtn, newChord)
     }
   }
 }
