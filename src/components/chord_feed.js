f4330e222f61dd94ea36baa050c9817aa4de0419
nathan-stamand <nathansaint810@gmail.com>
2020-07-06 13:39:39 -0500

    created #renderChordArray to create divs representing the chords in the feed and add them to their respective container

--- src/components/chord_feed.js
+++ src/components/chord_feed.js
@@ -2,7 +2,12 @@ class ChordFeed {
   constructor(feedJson) {
     this.id = feedJson.id;
     this.position = feedJson.position;
-    this.chord_array = feedJson.chord_array;
+    this.chord_array = feedJson.chord_array.split(', ');
     this.song_id = feedJson.song_id;
+  }
+
+  renderChordArray() {
+    const feedContainer = document.querySelector('song-feed-')
+    return this.chord_array.map(chord => `<div class='feed-chord'>${chord}</div>`).join('')
   }
 }
