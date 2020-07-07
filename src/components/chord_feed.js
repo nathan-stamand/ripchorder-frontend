3c5ab97c182a38e64a52c20a717b721101e5bef8
nathan-stamand <nathansaint810@gmail.com>
2020-07-07 13:22:48 -0500

    refactored #renderChordArray to createElement, so that event listener could be added for each. Also added a this.parent assignment in constructor as well as a parent parameter

--- src/components/chord_feed.js
+++ src/components/chord_feed.js
@@ -1,9 +1,10 @@
 class ChordFeed {
-  constructor(feedJson) {
+  constructor(feedJson, parent) {
     this.id = feedJson.id;
     this.position = feedJson.position;
     this.chord_array = feedJson.chord_array ? feedJson.chord_array.split(', ') : [];
     this.song_id = feedJson.song_id;
+    this.parent = parent;
   }
 
   isFull() {
@@ -12,10 +13,17 @@ class ChordFeed {
 
   addChordToFeed(chord) {
     this.chord_array.push(chord)
-    return this.renderChordArray()
   }
 
-  renderChordArray() {
-    return this.chord_array.map(chord => `<div class='feed-chord'>${chord}</div>`).join(' ')
+  renderChordArray(feedDiv) {
+    const chordFeed = this;
+    this.chord_array.map(chord => {
+      const feedChord = document.createElement('div');
+      feedChord.className = 'feed-chord';
+      feedChord.textContent = chord;
+      feedChord.id = feedDiv.childElementCount
+      chordFeed.addFeedChordEventListener(feedChord);
+      feedDiv.appendChild(feedChord)
+    })
   }
 }
