a7993bea7954248eb04aa36031585535103f1ade
nathan-stamand <nathansaint810@gmail.com>
2020-07-07 08:53:47 -0500

    added #isFull to determine when a feed is full of chords, added #addChordToFeed to add chord and render them to the feed

--- src/components/chord_feed.js
+++ src/components/chord_feed.js
@@ -2,8 +2,17 @@ class ChordFeed {
   constructor(feedJson) {
     this.id = feedJson.id;
     this.position = feedJson.position;
-    this.chord_array = feedJson.chord_array.split(', ');
+    this.chord_array = feedJson.chord_array ? feedJson.chord_array.split(', ') : [];
     this.song_id = feedJson.song_id;
+  }
+
+  isFull() {
+    return this.chord_array.length > 7 ? true : false;
+  }
+
+  addChordToFeed(chord) {
+    this.chord_array.push(chord)
+    return this.renderChordArray()
   }
 
   renderChordArray() {
