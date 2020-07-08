cb0704714982f972c9abc61f79e644f0e4479d58
nathan-stamand <nathansaint810@gmail.com>
2020-07-08 08:55:06 -0500

    defined and called #populateKey and #populateTempo to add options to the tempo/key select boxes

--- src/components/app.js
+++ src/components/app.js
@@ -15,10 +15,26 @@ class App {
     this.song = new Song(newSongParams)
     this.song.display();
     this.initBindingsAndListeners();
+    this.populateTempo();
+    this.populateKey();
   }
 
   initBindingsAndListeners() {
     this.playBtn = document.getElementById('play');
     this.playBtn.addEventListener('click', this.song.play.bind(this.song))
   }
+
+  populateTempo() {
+    for (let i = 60; i < 200; i++) {
+      $('#tempo').append(`<option value='${i}'>${i}</option>`)
+    } 
+  }
+
+  populateKey() {
+    const notes = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
+    for (const note of notes) {
+      $('#key').append(`<option value='${note}'>${note}</option>`)
+    }
+  }
+
 }
