3d99b5a7a6e7094d8dd7d785d59ac3a06bae34ca
nathan-stamand <nathansaint810@gmail.com>
2020-07-08 14:07:47 -0500

    replaced other definitions of songList and their instances with this.songList

--- src/components/songs.js
+++ src/components/songs.js
@@ -18,21 +18,20 @@ class Songs {
   }
 
   hiderUnhider(e) {
-    const songList = document.getElementById("song-list")
     let symbol = this.allSongs.getAttribute("data-symbol");
-    this.allSongsButtoner(songList, symbol)
+    this.allSongsButtoner(symbol)
   }
 
-  allSongsButtoner(songList, symbol) {
+  allSongsButtoner(symbol) {
     if (symbol === '▷') {
       this.allSongs.setAttribute('data-symbol', this.symbolSet['▷'])
       this.allSongs.textContent = `${this.symbolSet['▷']} ALL SONGS`
-      songList.hidden = false;
+      this.songList.hidden = false;
     }
     else {
       this.allSongs.setAttribute('data-symbol', this.symbolSet['▽'])
       this.allSongs.textContent = `${this.symbolSet['▽']} ALL SONGS`
-      songList.hidden = true;
+      this.songList.hidden = true;
     }
   }
 
@@ -43,8 +42,8 @@ class Songs {
   }
 
   liHider(song) {
-    const songList = $(`ul#song-list`).children().toArray()
-    for (const li of songList) {
+    const songListArray = $(`ul#song-list`).children().toArray()
+    for (const li of songListArray) {
       li.id === song.id ? li.hidden = true : li.hidden = false;
     }
   }
