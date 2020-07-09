b6553a756351d925e0b5fe2f5698be19b6a059b0
nathan-stamand <nathansaint810@gmail.com>
2020-07-09 11:18:28 -0500

    defined #variables to produce an object with most properties needed to save/create a song

--- src/adapters/SongAdapter.js
+++ src/adapters/SongAdapter.js
@@ -3,11 +3,22 @@ class SongAdapter {
     this.baseUrl = 'http://localhost:3000/songs/';
   }
 
+  variables() {
+    const tempo = $('#tempo option:selected').text()
+    const key = $('#key option:selected').text()
+    const mode = $('#mode option:selected').text()
+    const newTitle = $('#new-title').val()
+    const custMap = $('.custom.chord').toArray().map(chord => {
+      return `${chord.textContent}-${chord.getAttribute('octave')}`
+    }).join(', ');
+    return `{tempo: '${tempo}', key: '${key}', mode: '${mode}', title: '${newTitle}', custom_chords: '${custMap}'}`
+  }
+
   createSong() {
 
   }
 
   saveSong() {
-    
+
   }
 }
