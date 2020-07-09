e750d834b2aa0b868086acfe629b0ea92d70776f
nathan-stamand <nathansaint810@gmail.com>
2020-07-09 17:48:09 -0500

    assigned this.chordContainer to a new ChordContainer in constructor, removed #playChord, #playNote, #play, #sleep, #addStopStatus, #addPauseStatus, #chordsForPlay, #refreshStarterChord to be put in control_panel.js

--- src/components/song.js
+++ src/components/song.js
@@ -8,6 +8,7 @@ class Song {
     this.chordFeeds = new ChordFeeds(songJson.attributes.chord_feeds);
     this.customChords = songJson.attributes.custom_chords ? songJson.attributes.custom_chords.split(', ') : null;
     this.adapter = new SongAdapter;
+    this.chordContainer = new ChordContainer(this.key, this.customChords)
   }
 
   renderLi() {
@@ -32,19 +33,6 @@ class Song {
     this.renderTempoKeyMode()
     this.renderTitle()
     this.chordFeeds.refreshFeeds()
-  }
-
-  fetchAndSaveSong() {
-    let id = $('h1').attr('id')
-    if (id != 'null') {
-      let chordFeedVar = this.chordFeeds.variables(id)
-      this.adapter.saveSong(chordFeedVar)
-    }
-    else {
-      id = this.adapter.createSong('').then(res => res.data.id)
-      let chordFeedVar = this.chordFeeds.variables(id)
-      this.adapter.saveSong(chordFeedVar)
-    }
   }
 
   renderTitle() {
@@ -91,100 +79,6 @@ class Song {
       custChord.textContent = chord;
       this.addChordListener(custChord)
       $(custChord).insertBefore('#new-chord');
-    }
-  }
-
-  addChordListener(button) {
-    const song = this;
-    button.addEventListener('mousedown', function(e) {
-      song.playChord(button)
-      if (!song.chordFeeds.lastIsFull()) {
-        const lastFeed = song.chordFeeds.last()
-        lastFeed.addChordToFeed(button)
-      }
-      else {
-        if (!song.chordFeeds.full()) {
-          song.chordFeeds.addEmptyChordFeed()
-          const lastFeed = song.chordFeeds.last()
-          lastFeed.addChordToFeed(button)
-        }
-      }
-      song.chordFeeds.refreshFeeds()
-    })
-  }
-
-  playChord(button) {
-    Tone.start()
-    const note = button.textContent;
-    const octave = button.getAttribute('octave')
-    const notes = scribble.chord(`${note}-${octave}`)
-    notes.forEach(note => {
-      this.playNote(note)
-    })
-  }
-
-  playNote(note) {
-    Tone.Transport.bpm.value = $('#tempo option:selected').text();
-    const synth = new Tone.Synth().toDestination()
-    synth.triggerAttackRelease(note, '4n')
-  }
-
-  async play(e) {
-    Tone.start()
-    const chords = this.chordsForPlay()
-    for (let i = 0; i < chords.length; i++) {
-      this.waitTime = 60000/$('#tempo option:selected').text()
-      const status = document.getElementById('song-feed-container').getAttribute('status');
-      switch(status) {
-        case 'play':
-        if (i === chords.length - 1) {
-          this.chordFeeds.refreshFeeds()
-        }
-        this.playChord(chords[i])
-        await this.sleep(this.waitTime)
-      break;
-      case 'pause':
-        chords[i].setAttribute('start-chord', 'start')
-        this.addPauseStatus()
-        return;
-      default:
-        return;
-      }
-    }
-  }
-
-  sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-  async addStopStatus() {
-    $('#song-feed-container').attr('status', 'stop')
-    await this.sleep(this.waitTime)
-    $('#song-feed-container').attr('status', 'play')
-    this.chordFeeds.refreshFeeds()
-  }
-
-  chordsForPlay() {
-    let chords = $('div.feed-chord').toArray()
-    const startChord = chords.findIndex(chord => chord.getAttribute('start-chord') === 'start')
-    if (startChord > 0) {
-      chords = chords.slice(startChord)
-    }
-    return chords;
-  }
-
-  async addPauseStatus() {
-    $('#song-feed-container').attr('status', 'pause')
-    await this.sleep(this.waitTime)
-    $('#song-feed-container').attr('status', 'play')
-    this.refreshStartChord()
-  }
-
-  refreshStartChord() {
-    let chords = this.chordsForPlay()
-    let startChords = chords.filter(chord => chord.getAttribute('start-chord') === 'start')
-    if (startChords.length > 1) {
-      startChords[0].setAttribute('start-chord', 'false')
     }
   }
 }
