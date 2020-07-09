321d7ef3e4a1ba8f509b9cfdbd2f46463cd6bc3c
nathan-stamand <nathansaint810@gmail.com>
2020-07-09 17:41:29 -0500

    called #fetchCreateSong upon construction, set this.controlPanel to create new ControlPanel

--- src/components/app.js
+++ src/components/app.js
@@ -1,19 +1,7 @@
 class App {
   constructor() {
     this.songs = new Songs(this);
-    const blankChordFeed = {id: null, position: 1, chord_array: null}
-    const newSongParams = {
-      id: null, attributes: {
-        title: 'New Song', 
-        key: 'C', 
-        mode: 'major', 
-        tempo: 120, 
-        custom_chords: null, 
-        chord_feeds: [blankChordFeed]
-      }
-    }
-    this.song = new Song(newSongParams)
-    this.controlPanel = new ControlPanel(this)
-    this.song.display();
+    this.songs.fetchCreateSong()
+    this.controlPanel = new ControlPanel(this);
   }
 }
