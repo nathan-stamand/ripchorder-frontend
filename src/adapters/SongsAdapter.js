b739685012d98ed973c3ac5d7e34af7c5da43507
nathan-stamand <nathansaint810@gmail.com>
2020-07-09 17:39:43 -0500

    added #variables  and #createSong, modified #variables to return object of default settings

--- src/adapters/SongsAdapter.js
+++ src/adapters/SongsAdapter.js
@@ -6,4 +6,20 @@ class SongsAdapter {
   getSongs() {
     return fetch(this.baseUrl).then(res => res.json())
   }
+
+  variables() {
+    return {song: {tempo: 120, key: 'C', mode: 'major', title: `New Song`, custom_chords: '', chord_feeds: []}}
+  }
+
+  createSong() {
+    const songObject = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.variables())
+    }
+    return fetch(this.baseUrl, songObject).then(res => res.json())
+  }
 }
