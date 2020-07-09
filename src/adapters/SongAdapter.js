cce74019ae67697affd1629709f75a5de31fc18d
nathan-stamand <nathansaint810@gmail.com>
2020-07-09 17:37:09 -0500

    removed all methods

--- src/adapters/SongAdapter.js
+++ src/adapters/SongAdapter.js
@@ -1,29 +1,5 @@
 class SongAdapter {
   constructor() {
     this.baseUrl = 'http://localhost:3000/songs';
-    this.variables.bind(this)
-  }
-
-  variables(chordFeeds) {
-    const tempo = $('#tempo option:selected').text()
-    const key = $('#key option:selected').text()
-    const mode = $('#mode option:selected').text()
-    const newTitle = $('#new-title').val()
-    const custMap = $('.custom.chord').toArray().map(chord => {
-      return `${chord.textContent}-${chord.getAttribute('octave')}`
-    }).join(', ');
-    return {song: {tempo: `${tempo}`, key: `${key}`, mode: `${mode}`, title: `${newTitle}`, custom_chords: `${custMap}`, chord_feeds: `${chordFeeds}`}}
-  }
-
-  createSong(chordFeedsVar) {
-    const songObject = {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(this.variables(chordFeedsVar))
-    }
-    return fetch(this.baseUrl, songObject).then(res => res.json())
   }
 }
