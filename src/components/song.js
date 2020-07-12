37dc4f2310cf7cd3fdd1160f40afa7371a132e2a
nathan-stamand <nathansaint810@gmail.com>
2020-07-12 16:15:46 -0500

    changed return value of #renderito have child div.button-container, mdeleted #createShowDeletebuttons

--- src/components/song.js
+++ src/components/song.js
@@ -12,17 +12,7 @@ class Song {
   }
 
   renderLi() {
-    return `<li class="song" id="${this.id}">${this.title}</li>`
-  }
-
-  static createShowDeleteButtons() {
-    const songList = document.getElementsByClassName('song')
-    for (const song of songList) {
-      const title = song.innerHTML
-      const deleteBtn = `<button class='delete-button' data-song='${song.id}'>DELETE</button>`
-      const showBtn = `<button class='show-button' data-song='${song.id}'>SHOW</button>`
-      song.innerHTML = `${title}<div class='button-container' id='${song.id}'>${deleteBtn}${showBtn}</div>`
-    }
+    return `<li class="song" id="${this.id}">${this.title}<div class='button-container'></div></li>`
   }
 
   renderChords() {
