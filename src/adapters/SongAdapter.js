3ab0164365e518c87a71213bd598b5d44b7d27bf
nathan-stamand <nathansaint810@gmail.com>
2020-07-13 10:00:25 -0500

    created #saveSong to create const songObj and call fetch to update the song, created #songVariables to help create the body of the songObj in #savesong

--- src/adapters/SongAdapter.js
+++ src/adapters/SongAdapter.js
@@ -2,4 +2,21 @@ class SongAdapter {
   constructor() {
     this.baseUrl = 'http://localhost:3000/songs';
   }
+
+  saveVariables(songId) {
+    // return {song: {title: title, key: key, mode: mode, tempo: tempo, custom_chords: customChords, chord_feeds: chordFeeds }}
+  }
+
+  saveSong(songId) {
+    const songObj = {
+      method: "PATCH",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.saveVariables(songId))
+    }
+
+    return fetch(this.baseUrl + `/${songId}`, songObj).then(res => console.log(res))
+  }
 }
