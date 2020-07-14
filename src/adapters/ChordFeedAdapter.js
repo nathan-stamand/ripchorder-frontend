6e91d5a2fbae457bdd1cb915ac1a2974d003af46
nathan-stamand <nathansaint810@gmail.com>
2020-07-14 13:20:44 -0500

    deleted #deleteChordFeed

--- src/adapters/ChordFeedAdapter.js
+++ src/adapters/ChordFeedAdapter.js
@@ -2,16 +2,4 @@ class ChordFeedAdapter {
   constructor() {
     this.baseUrl = 'http://localhost:3000/chord_feeds'
   }
-
-  // deleteChordFeed(id) {
-  //   const feedObj = {
-  //     method: "DELETE",
-  //     headers: {
-  //       'Accept': 'application/json',
-  //       'Content-Type': 'application/json'
-  //     },
-  //     body: JSON.stringify({id: id})
-  //   }
-  //   return fetch(this.baseUrl + `/${id}`, feedObj).then(res => res.json())
-  // }
 }
