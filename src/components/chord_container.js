54d2f14a3ddbaae10c0a1b2d25789ee44dc335d5
nathan-stamand <nathansaint810@gmail.com>
2020-07-14 13:30:20 -0500

    removed conditional that removed empty chordFeeds from #addFeedChordEventListener

--- src/components/chord_container.js
+++ src/components/chord_container.js
@@ -40,10 +40,6 @@ class ChordContainer {
   addFeedChordEventListener(feedChord) {
     feedChord.addEventListener('mousedown', function(e) {
       feedChord.remove()
-      const feeds = $('.feed').toArray()
-      for (let i = 0; i != feeds.length; i++) {
-        feeds[i].children.length < 1 ? feeds[i].remove() : feeds[i].id = i + 1
-      }
     })
   }
 
