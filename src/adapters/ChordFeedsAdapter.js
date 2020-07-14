a73cd1e4350775b4530ce15122d31fd227821da7
nathan-stamand <nathansaint810@gmail.com>
2020-07-14 13:12:19 -0500

    commented out #createFeed

--- src/adapters/ChordFeedsAdapter.js
+++ src/adapters/ChordFeedsAdapter.js
@@ -3,15 +3,15 @@ class ChordFeedsAdapter {
     this.baseUrl = 'http://localhost:3000/chord_feeds'
   }
 
-  createFeed(feedData) {
-    const feedObj = {
-      method: "POST",
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({chord_feed: feedData})
-    }
-    return fetch(this.baseUrl, feedObj).then(res => console.log(res.json()))
-  }
+  // createFeed(feedData) {
+  //   const feedObj = {
+  //     method: "POST",
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({chord_feed: feedData})
+  //   }
+  //   return fetch(this.baseUrl, feedObj).then(res => res.json())
+  // }
 }
