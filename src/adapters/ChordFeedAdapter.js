90dab4a7230beeb70229c9f7ce2d9dc0b3384dfe
nathan-stamand <nathansaint810@gmail.com>
2020-07-14 18:43:13 -0500

    created #setChordFeedBody and #updatechordFeed

--- src/adapters/ChordFeedAdapter.js
+++ src/adapters/ChordFeedAdapter.js
@@ -3,7 +3,24 @@ class ChordFeedAdapter {
     this.baseUrl = 'http://localhost:3000/chord_feeds'
   }
 
-  updateChordFeed() {
+  setChordFeedBody(positon) {
+    const feed = $(`div#${positon}.feed`)
+    const chordArray = feed.children().toArray()
+    const stringChordArray = chordArray.map(chord => chord.textContent).join(', ')
+    const chordFeedBody = {chord_feed: {position: positon, chord_array: stringChordArray}}
+    return chordFeedBody
+  }
 
+  updateChordFeed(position, id) {
+    const chordFeedBody = this.setChordFeedBody(position)
+    const chordFeedObj = {
+      method: "PATCH",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(chordFeedBody)
+    }
+    return fetch(this.baseUrl + `/${id}`, chordFeedObj).then(res => res.json()).then(res => console.log(res))
   }
 }
