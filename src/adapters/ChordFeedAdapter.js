e63ce0773b1c4fcdc261949bacdf00dfff5c0110
nathan-stamand <nathansaint810@gmail.com>
2020-07-14 13:19:15 -0500

    commented out #deletecChordFeed

--- src/adapters/ChordFeedAdapter.js
+++ src/adapters/ChordFeedAdapter.js
@@ -3,15 +3,15 @@ class ChordFeedAdapter {
     this.baseUrl = 'http://localhost:3000/chord_feeds'
   }
 
-  deleteChordFeed(id) {
-    const feedObj = {
-      method: "DELETE",
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: null
-    }
-    return fetch(this.baseUrl + `/${id}`, feedObj).then(res => console.log(res))
-  }
+  // deleteChordFeed(id) {
+  //   const feedObj = {
+  //     method: "DELETE",
+  //     headers: {
+  //       'Accept': 'application/json',
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({id: id})
+  //   }
+  //   return fetch(this.baseUrl + `/${id}`, feedObj).then(res => res.json())
+  // }
 }
